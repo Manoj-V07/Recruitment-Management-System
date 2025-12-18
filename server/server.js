401014364e91c8a35f4bc033eeb93ddb3ae9b6ad
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-12-18 12:11:52 +0530

    feat : basic api running

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/' , (req,res) => {
+    res.status(200).send('API is running...');
+})
+
+app.listen(process.env.PORT , () => {
+    console.log('Server is running on port 5000');
+});
