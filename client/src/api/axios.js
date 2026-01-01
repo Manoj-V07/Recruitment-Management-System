94c269a9e7e46428d2a8bf8391a35019f749e22c
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2026-01-01 16:06:35 +0530

    updated the url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://recruitment-management-system-3-nvub.onrender.com/',
+  baseURL: import.meta.env.VITE_API_URL || 'https://recruitment-management-system-cb3j.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
