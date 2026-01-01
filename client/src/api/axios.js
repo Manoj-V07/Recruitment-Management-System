85e16f74543e1ca33acefe64330ccf4301fde249
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2026-01-01 14:59:36 +0530

    api url changed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://recruitment-management-system-3-nvub.onrender.com',
+  baseURL: 'https://recruitment-management-system-3-nvub.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
