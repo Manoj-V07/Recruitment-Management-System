48f844df2351868dc71cac02fa990b6ecaec852d
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2026-01-30 22:00:57 +0530

    added the vite_api_url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:  'https://recruitment-management-system-1-btao.onrender.com/',
+  baseURL:  import.meta.env.VITE_API_URL || 'http://localhost:5000/',
   headers: {
     'Content-Type': 'application/json',
   },
